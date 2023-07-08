ef6910a44224cdaa230340bed8023a3ca6777997
786Nurjaman <nurjamanshekh@gmail.com>
2023-07-08 18:12:30 +0530

    map added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -695,48 +695,47 @@ export default function Home() {
     </div>
   </div> */}
 
-  <div id="contact" className="contact-us section">
-    <div className="containerform">
-      <div className="row">
-        <div className="col-lg-7">
-          <div className="section-heading align-self-center">
-            <h2>Feel free to <em>Contact</em> us</h2>
+<div id="contact" class="contact-us section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-7">
+          <div class="section-heading">
+            <h2>Feel free to <em>Contact</em> us via the <span>HTML form</span></h2>
             <div id="map">
+              <iframe src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" style={{width:"100%", height:"360px", frameborder:"0",allowfullscreen:""}}></iframe>
             </div>
-            <div ><p className="paragraph">"We are open to everyone who wishes to discuss their business needs, and we are dedicated to providing quality service. We welcome you to join us!"</p></div>
-
-            <div className="info">
-              <span><i className="fa fa-phone"></i><a href="#">010-020-0340<br/>090-080-0760</a></span>
-              <span><i className="fa fa-envelope"></i><a href="#">info@company.com<br/>mail@company.com</a></span>
+            <div class="info">
+              <span><i class="fa fa-phone"></i> <a href="#">010-020-0340<br/>090-080-0760</a></span>
+              <span><i class="fa fa-envelope"></i> <a href="#">info@company.com<br/>mail@company.com</a></span>
             </div>
           </div>
         </div>
-        <div className="col-lg-5 align-self-center">
+        <div class="col-lg-5 align-self-center">
           <form id="contact" action="" method="get">
-            <div className="row">
-              <div className="col-lg-12">
+            <div class="row">
+              <div class="col-lg-12">
                 <fieldset>
                   <input type="name" name="name" id="name" placeholder="Name" autocomplete="on" required/>
                 </fieldset>
               </div>
-              <div className="col-lg-12">
+              <div class="col-lg-12">
                 <fieldset>
                   <input type="surname" name="surname" id="surname" placeholder="Surname" autocomplete="on" required/>
                 </fieldset>
               </div>
-              <div className="col-lg-12">
+              <div class="col-lg-12">
                 <fieldset>
                   <input type="text" name="email" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email" required=""/>
                 </fieldset>
               </div>
               <div className="col-lg-12">
-              <fieldset>
-              <textarea name="message" id="message" placeholder="Your Message" required></textarea>
-            </fieldset>
-              </div>
-              <div className="col-lg-12">
                 <fieldset>
-                  <button type="submit" id="form-submit" className="main-button">Submit Request</button>
+                  <textarea name="message" id="message" placeholder="Your Message" required></textarea>
+                </fieldset>
+                </div>
+              <div class="col-lg-12">
+                <fieldset>
+                  <button type="submit" id="form-submit" class="main-button">Submit Request</button>
                 </fieldset>
               </div>
             </div>
@@ -744,10 +743,10 @@ export default function Home() {
         </div>
       </div>
     </div>
-    <div className="contact-dec">
+    <div class="contact-dec">
       <img src="assets/images/contact-dec.png" alt=""/>
-    </div> 
-     <div className="contact-left-dec">
+    </div>
+    <div class="contact-left-dec">
       <img src="assets/images/contact-left-dec.png" alt=""/>
     </div>
   </div>
